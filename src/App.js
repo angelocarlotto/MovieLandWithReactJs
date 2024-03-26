3beab8eb89d4bd3c5ab4e16137ee34bfc9d4ed5f
Angelo Carlotto <angelocarlotto@gmail.com>
2024-03-25 20:42:06 -0400

    sdasdsad

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import "./App.css";
+const Person = (props) => {
+  return (
+    <>
+      <h1>Name: {props.name}</h1>
+      <h2>Last name: {props.lastName}</h2>
+      <h2>Age: {props.age}</h2>
+    </>
+  );
+};
 
-function App() {
+const Counter = () => {
+  const [count, setCount] = useState(2);
+
+  useEffect(() => {
+    console.log(count);
+  }, [count]);
+  return (
+    <>
+      <div>
+        <button style={{ display: "inline" }} onClick={() => setCount(count - 1)}>
+          -
+        </button>
+        <h1 style={{ display: "inline" }}>{count}</h1>
+        <button style={{ display: "inline" }} onClick={() => setCount(count + 1)}>
+          +
+        </button>
+      </div>
+    </>
+  );
+};
+
+const App = () => {
+  const name = "John";
+  const isNameShowing = false;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello, {isNameShowing ? name : "Some one"}! XD</h1>
+      {name ? (
+        <>
+          <Person name={"John 2"} age="33" />
+          <Person name={name} />
+          <Person name={"Maria"} />
+          <Counter />
+          <Counter />
+          <Counter />
+          <Counter />
+        </>
+      ) : (
+        <>
+          <h2>there is no name</h2>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default App;
